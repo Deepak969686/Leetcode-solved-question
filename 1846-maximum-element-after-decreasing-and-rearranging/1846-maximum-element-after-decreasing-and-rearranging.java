b7afe3e3e7918ce375425b1fd4eec29260917d50
Deepak Kumar Saini <157050176+Deepak969686@users.noreply.github.com>
2026-06-29 23:06:31 +0530

    Time: 10 ms (29.67%), Space: 77.4 MB (61.38%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.java
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.java
@@ -14,3 +14,39 @@ class Solution {
         return max;
     }
 }
+
+// others
+
+
+// class Solution {
+//     public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+
+//         Arrays.sort(arr);
+
+//         arr[0] = 1;
+
+//         for (int i = 1; i < arr.length; i++) {
+
+//             arr[i] = Math.min(arr[i], arr[i - 1] + 1);
+
+//         }
+
+//         return arr[arr.length - 1];
+//     }
+// }
+
+
+
+// class Solution {
+//     public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+//         Arrays.sort(arr);
+//         int m=1;
+//         for(int i=1;i<arr.length;i++){
+//             if(arr[i]>m){
+//             m+=1;
+//             }
+//         }
+//         return m;
+        
+//     }
+// }
