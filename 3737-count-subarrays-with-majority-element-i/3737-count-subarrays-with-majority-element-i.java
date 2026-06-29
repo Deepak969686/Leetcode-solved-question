cc08eb9a9ca2634b00190670a75fa51625b94d15
Deepak Kumar Saini <157050176+Deepak969686@users.noreply.github.com>
2026-06-29 22:42:40 +0530

    Time: 5 ms (92.14%), Space: 47.1 MB (36.58%) - LeetHub

--- 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.java
+++ 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.java
@@ -1,3 +1,4 @@
+//optimize
 class Solution {
     public int countMajoritySubarrays(int[] nums, int target) {
         int n=nums.length;
@@ -21,3 +22,25 @@ class Solution {
         return res;
     }
 }
+
+// brute force 
+// int res=0;
+// for(int i=0;i<n;i++){
+//     if(nums[i]==target){
+//         nums[i]=1;
+//     } else{
+//         nums[i]=-1;
+//     }
+// }
+// for(int i=1;i<n;i++){
+//     nums[i]+=nums[i-1];
+// }
+// for(int i=0;i<n;i++){
+//     for(int j=i;j<n;j++){
+//         int sum=nums[j]-((i>0)?nums[i-1]:0);
+//         if(sum>0){
+//             res++;
+//         }
+//     }
+// }
+// return res;
